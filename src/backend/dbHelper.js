f6fa00860d516360185289a20e6096c330f0a9bc
bradley <bradley.plater1@gmail.com>
2019-11-19 15:40:59 +0000

    Added query back

--- src/backend/dbHelper.js
+++ src/backend/dbHelper.js
@@ -4,6 +4,23 @@ const crypto = require('crypto');
 const USER_TABLE = 'User';
 
 
+function query(statement) {
+  const connection = mysql.createConnection({
+    host: 'database-3.cdqdrq2hjhze.eu-west-2.rds.amazonaws.com',
+    user: 'admin',
+    password: 'thelegend27',
+    database: 'makersbnb'
+});
+
+  return new Promise((resolve, reject) => {
+    connection.query(statement, (error, result) => {
+      if (error) return reject(error);
+      resolve(result);
+    });
+  });
+}
+
+
 function authenticateQuery(email) {
   const connection = mysql.createConnection({
     host: 'database-3.cdqdrq2hjhze.eu-west-2.rds.amazonaws.com',
