237ece3ec83cee8c1efa00832ffbd6062a01292c
6joeb <joe6ball@gmail.com>
2019-11-20 11:12:04 +0000

    added checkPassword function

--- src/backend/User.js
+++ src/backend/User.js
@@ -1,6 +1,7 @@
 const crypto = require('crypto');
 const { authenticateQuery } = require('./dbHelper');
 const { registerQuery } = require('./dbHelper');
+var errorMessage;
 
 class User {
   constructor(id, firstName, lastName, email) {
@@ -36,16 +37,30 @@ class User {
   */
   static async register(firstName, lastName, email, password, confirmPassword) {
     if (firstName != "" && lastName != "" && email != "" && email.includes("@") && password != "" && confirmPassword != "") {
-      if (password === confirmPassword) {
+      if (checkPassword(password, confirmPassword) === true) {
         const result = await registerQuery(firstName, lastName, email, password);
         return new User(result.insertId, firstName, lastName, email);
       } else {
-        throw ("Passwords don't match");
+        throw (errorMessage);
       }
     } else {
       throw ("Field left empty")
     }
   }
 };
+
+function checkPassword(password, confirmPassword) {
+  if (password.length < 7) {
+    (errorMessage = "Too short, 8 char min");
+  } else if (password.length > 30) {
+    (errorMessage = "Too long, 30 char max");
+  } else if (password.search(/\d/) == -1) {
+    (errorMessage = "No number(s)");
+  } else if (password.search(/[a-zA-Z]/) == -1) {
+    (errorMessage = "No letter(s)");
+  } else if (password !== confirmPassword) {
+    (errorMessage = "Passwords do not match")
+  } else { return true; }
+}
 
 module.exports = User;
