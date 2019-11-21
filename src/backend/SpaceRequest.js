0a5eb91795a0f8f493448a7d8dc6ef310b09ec0e
DMartinBGL <david.martin@bglgroup.co.uk>
2019-11-21 10:33:19 +0000

    spaceRequest db Methods

--- src/backend/SpaceRequest.js
+++ src/backend/SpaceRequest.js
@@ -1,13 +1,52 @@
+const { query } = require('./dbHelper');
+
 class SpaceRequest {
-  constructor(id, requestingUser, startDate, endDate, owner, available, dateCreated,) {
+  constructor(id, requestingUser, startDate, endDate, owner, status, spaceId) {
 
     this.id = id;
     this.requestingUser = requestingUser;
     this.startDate = startDate;
     this.endDate = endDate;
     this.owner = owner;
-    this.available = available;
-    this.dateCreated = dateCreated;
+    this.status = status;
+    this.spaceId = spaceId;
+  }
+
+  static async listAll() {
+    let list = [];
+    const result = await query('SELECT * FROM SpaceRequest'); 
+    result.forEach(data => {
+      list.push(new SpaceRequest(data.id, data.requestinguser, data.startdate, data.enddate, data.owner, data.status, data.spaceid));
+    });
+    return list;
+  }
+
+  static async add(requestingUser, startDate, endDate, owner, status, spaceId) {
+    const result = await query(`INSERT INTO SpaceRequest( requestinguser, startdate, enddate, owner, status, spaceid) 
+                        VALUES('${requestingUser}','${startDate}', '${endDate}', '${owner}', '${status}', ${spaceId})`);
+    return new SpaceRequest(result.insertId, requestingUser, startDate, endDate, owner, status, spaceId);                    
+  }
+
+  static async listByRequester(requestingUser) {
+    let list = [];
+    const result = await query(`SELECT * FROM SpaceRequest WHERE requestinguser = '${requestingUser}'`); 
+    result.forEach(data => {
+      list.push(new SpaceRequest(data.id, data.requestinguser, data.startdate, data.enddate, data.owner, data.status, data.spaceid));
+    });
+    return list;
+  }
+
+  static async listByOwner(owner) {
+    let list = [];
+    const result = await query(`SELECT * FROM SpaceRequest WHERE owner = '${owner}'`); 
+    result.forEach(data => {
+      list.push(new SpaceRequest(data.id, data.requestinguser, data.startdate, data.enddate, data.owner, data.status, data.spaceid));
+    });
+    return list;
+  }
+
+  static async setStatus(id, newStatus) {
+    await query(`UPDATE SpaceRequest SET status = '${newStatus}' WHERE id = ${id}`);
   }
 }
 
